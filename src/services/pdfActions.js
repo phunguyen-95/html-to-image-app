f8ac1073a147bc08a85341ef0253c88261368851
phu.nguyen <phu.nguyen@blueberrylabs.com>
2025-03-19 11:08:34 +0700

    [Github deploy]

--- src/services/pdfActions.js
+++ src/services/pdfActions.js
@@ -1,5 +1,22 @@
 import axios from "axios";
 
+function blobToPDF(data, fileName = "mergePDF.pdf") {
+  // Convert ArrayBuffer to Blob
+  const blob = new Blob([data], { type: "application/pdf" });
+  const url = URL.createObjectURL(blob);
+
+  // Create a download link and trigger download
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url); // Cleanup
+
+  console.log("✅ PDF downloaded successfully.");
+}
+
 const uploadFiles = async (files) => {
   if (!files.length) {
     alert("No files selected!");
@@ -15,18 +32,25 @@ const uploadFiles = async (files) => {
 
   try {
     const response = await axios.post(
-      "https://stirlingpdf.io/api/v1/general/merge-pdfs",
+      "https://ai-pdf-service.survivalapp.com/api/v1/general/merge-pdfs",
       formData,
       {
         headers: {
-          "Content-Type": "multipart/form-data",
-          origin: "https://stirlingpdf.io"
-        }
+          "Content-Type": "multipart/form-data"
+        },
+        responseType: "blob"
       }
     );
+    let fileName = "";
+    files.forEach((file) => {
+      fileName += file.name.replace(".pdf", "") + "_";
+    });
 
-    console.log("Upload Successful:", response.data);
-    alert("Files uploaded successfully!");
+    const contentType = response.headers["content-type"];
+    if (!contentType || !contentType.includes("application/pdf")) {
+      throw new Error("Invalid PDF file received.");
+    }
+    blobToPDF(response.data, `${fileName}.pdf`);
   } catch (error) {
     console.error("Upload Error:", error);
     alert("Error uploading files!");
